7a163ca4bc7ba843559476a24f436132e43d8936
DeaDTriX <97814611+DeaDTriX1@users.noreply.github.com>
2022-08-21 17:41:22 +0200

    Add files via upload

---
+++ function.lua
@@ -0,0 +1,139 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(10)
+    end
+    while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(10)
+    end
+    if ESX.IsPlayerLoaded() then
+
+		ESX.PlayerData = ESX.GetPlayerData()
+
+    end
+end)
+
+
+function ApplySkin(infos)
+    TriggerEvent('skinchanger:getSkin', function(skin)
+        local uniformObject
+
+        if skin.sex == 0 then
+            uniformObject = infos.variations.male
+        else
+            uniformObject = infos.variations.female
+        end
+
+        if uniformObject then
+            TriggerEvent('skinchanger:loadClothes', skin, uniformObject)
+        end
+
+        infos.onEquip()
+    end)
+end
+
+function SpawnObj(obj)
+    local playerPed = PlayerPedId()
+    local coords, forward = GetEntityCoords(playerPed), GetEntityForwardVector(playerPed)
+    local objectCoords = (coords + forward * 1.0)
+    local Ent = nil
+
+    SpawnObject(obj, objectCoords, function(obj)
+        SetEntityCoords(obj, objectCoords, 0.0, 0.0, 0.0, 0)
+        SetEntityHeading(obj, GetEntityHeading(playerPed))
+        PlaceObjectOnGroundProperly(obj)
+        Ent = obj
+        Wait(1)
+    end)
+    Wait(1)
+    while Ent == nil do Wait(1) end
+    SetEntityHeading(Ent, GetEntityHeading(playerPed))
+    PlaceObjectOnGroundProperly(Ent)
+    local placed = false
+    while not placed do
+        Citizen.Wait(1)
+        local coords, forward = GetEntityCoords(playerPed), GetEntityForwardVector(playerPed)
+        local objectCoords = (coords + forward * 2.0)
+        SetEntityCoords(Ent, objectCoords, 0.0, 0.0, 0.0, 0)
+        SetEntityHeading(Ent, GetEntityHeading(playerPed))
+        PlaceObjectOnGroundProperly(Ent)
+        SetEntityAlpha(Ent, 170, 170)
+
+        if IsControlJustReleased(1, 38) then
+            placed = true
+        end
+    end
+
+    FreezeEntityPosition(Ent, true)
+    SetEntityInvincible(Ent, true)
+    ResetEntityAlpha(Ent)
+    local NetId = NetworkGetNetworkIdFromEntity(Ent)
+    table.insert(object, NetId)
+
+end
+
+function SpawnObject(model, coords, cb)
+    local model = GetHashKey(model)
+
+    Citizen.CreateThread(function()
+        RequestModels(model)
+        Wait(1)
+        local obj = CreateObject(model, coords.x, coords.y, coords.z, true, false, true)
+
+        if cb then
+            cb(obj)
+        end
+    end)
+end
+
+function RequestModels(modelHash)
+    if not HasModelLoaded(modelHash) and IsModelInCdimage(modelHash) then
+        RequestModel(modelHash)
+
+        while not HasModelLoaded(modelHash) do
+            Citizen.Wait(1)
+        end
+    end
+end
+
+function RemoveObj(id, k)
+    Citizen.CreateThread(function()
+        SetNetworkIdCanMigrate(id, true)
+        local entity = NetworkGetEntityFromNetworkId(id)
+        NetworkRequestControlOfEntity(entity)
+        local test = 0
+        while test > 100 and not NetworkHasControlOfEntity(entity) do
+            NetworkRequestControlOfEntity(entity)
+            Wait(1)
+            test = test + 1
+        end
+        SetEntityAsNoLongerNeeded(entity)
+
+        local test = 0
+        while test < 100 and DoesEntityExist(entity) do 
+            SetEntityAsNoLongerNeeded(entity)
+            TriggerServerEvent("DeleteEntity", NetworkGetNetworkIdFromEntity(entity))
+            DeleteEntity(entity)
+            DeleteObject(entity)
+            if not DoesEntityExist(entity) then 
+                table.remove(object, k)
+            end
+            SetEntityCoords(entity, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0)
+            Wait(1)
+            test = test + 1
+        end
+    end)
+end
+
+function GoodName(hash)
+    if hash == GetHashKey("prop_medstation_04") then
+        return "Cone"
+    elseif hash == GetHashKey("prop_barrier_work05") then
+        return "Barrière"
+    else
+        return hash
+    end
+
+end
